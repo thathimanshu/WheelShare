2201bd3098458f1b760173fb9bc87f217f560b9a
thathimanshu <thathimanshusaini@gmail.com>
2024-04-04 12:11:28 +0530

    till reviews

--- app.js
+++ app.js
@@ -2,16 +2,43 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
-const mongoose_url = 'mongodb://127.0.0.1:27017/wonderlust'
+const Review = require("./models/review.js");
+const mongoose_url = 'mongodb://127.0.0.1:27017/wonderlust';
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync');
+const ExpressError = require('./utils/ExpressError');
+const {listingSchema, reviewSchema} = require('./schema.js');
+
 app.use(methodOverride('_method'));
 app.set("views",path.join(__dirname,"/views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));
 app.engine('ejs',ejsMate);
+
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(", ");
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+};
+
+const validateReview = (req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(", ");
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+};
 
 async function main(){
     await mongoose.connect(mongoose_url);
@@ -26,6 +53,7 @@ main().then(()=>{
 app.listen(8080,()=>{
     console.log("App is Listening");
 });
+
 
 app.get('/',(req,res)=>{
     res.send("Root path");
@@ -42,17 +70,17 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
 //Show route
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id} = req.params;
-    let listing = await Listing.findById(id);
+    let listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
-});
+}));
 //Create route
-app.post("/listings",async (req,res)=>{
+app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}) );
 
 //Edit route
 app.get("/listings/:id/edit",async (req,res)=>{
@@ -61,11 +89,11 @@ app.get("/listings/:id/edit",async (req,res)=>{
     res.render("listings/edit.ejs",{listing});  
 });
 //Update route
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
     let {id} = req.params;
     listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete route
 app.delete("/listing/:id",async (req,res)=>{
@@ -73,4 +101,38 @@ app.delete("/listing/:id",async (req,res)=>{
     let deleted = await Listing.findByIdAndDelete(id);
     console.log(deleted);
     res.redirect("/listings");
+});
+
+//REVIEWSS
+
+//POST review route
+app.post("/listings/:id/reviews",validateReview,wrapAsync( async (req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found"));
+})
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something Went Wrong"} = err;
+    res.status(statusCode).render("error.ejs",{err});
+    //res.status(statusCode).send(message);
 });
