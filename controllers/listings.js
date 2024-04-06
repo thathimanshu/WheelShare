cddc1d143ee9debc94ef8b9d7ffe5e5d9291a51d
thathimanshu <thathimanshusaini@gmail.com>
2024-04-06 15:35:14 +0530

    before maps

--- controllers/listings.js
+++ controllers/listings.js
@@ -24,8 +24,11 @@ module.exports.showListing = async (req,res)=>{
     res.render("listings/show.ejs",{listing});
 };
 module.exports.createListing = async (req,res,next)=>{
+    let url = req.file.path;
+    let fileName = req.file.filename;
     let newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url,fileName};
     await newListing.save();
     req.flash("success","New Listing Created!");
     res.redirect("/listings");
@@ -38,11 +41,19 @@ module.exports.renderEditForm = async (req,res)=>{
         req.flash("error","Listing does not exists");
         return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs",{listing});  
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace('/upload','/upload/w_250');
+    res.render("listings/edit.ejs",{listing,originalImageUrl});  
 };
 module.exports.updateListing = async (req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    if(typeof req.file !== 'undefined'){
+        let url = req.file.path;
+        let fileName = req.file.filename;
+        listing.image = {url,fileName};
+        listing.save();
+    }
     req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
--- routes/listing.js
+++ routes/listing.js
@@ -4,12 +4,18 @@ const wrapAsync = require('../utils/wrapAsync');
 const {isLoggedIn,isOwner,validateListing} = require('../middleware.js');
 const listingController = require('../controllers/listings.js');
 const multer = require('multer');
-const upload = multer({dest:'uploads/'});
+const {storage} = require('../cloudConfig.js');
+const upload = multer({storage});
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  )
 
 //New route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
@@ -17,7 +23,13 @@ router.get("/new",isLoggedIn,listingController.renderNewForm);
 router
   .route("/:id")
   .get(wrapAsync(listingController.showListing))
-  .put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
   .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
 //Edit route
